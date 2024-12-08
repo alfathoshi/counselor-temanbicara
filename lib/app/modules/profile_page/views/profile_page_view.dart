82e1566b661f5b1bc8bf1aa262527aab6be7e527
Raga-Git <ragadhitya@gmail.com>
2024-12-09 00:08:37 +0700

    add: profile-page

--- lib/app/modules/profile_page/views/profile_page_view.dart
+++ lib/app/modules/profile_page/views/profile_page_view.dart
@@ -1,21 +1,189 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../themes/colors.dart';
+import '../../../themes/fonts.dart';
 import '../controllers/profile_page_controller.dart';
 
 class ProfilePageView extends GetView<ProfilePageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ProfilePageView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProfilePageView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: whiteColor,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(32, 65, 32, 32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundColor: border,
+                        child: CircleAvatar(
+                          radius: 58,
+                          backgroundColor: whiteColor,
+                          child: Image.asset(
+                            'assets/images/profile_picture.png',scale: 2,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 30,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: primaryColor,
+                        ),
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.edit,
+                            size: 16,
+                            color: whiteColor,
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Astro',
+                    style: h3Bold,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Account Settings',
+                  style: h4Regular,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Edit Profile',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Change Password',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Change Language',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Available Schedule',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 32,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'More',
+                      style: h4Regular,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'About us',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Privacy Policy',
+                      style: h4SemiBold,
+                    ),
+                    Icon(
+                      Icons.chevron_right_outlined,
+                      size: 32,
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
